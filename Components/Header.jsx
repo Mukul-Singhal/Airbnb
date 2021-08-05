94a31586cb40aeaaf9f461ebc03c0e8538e936d7
Mukul Singhal <mukulsinghal98@gmail.com>
2021-08-05 22:18:03 +0530

    Initial Commit

---
+++ Components/Header.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div className="">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
